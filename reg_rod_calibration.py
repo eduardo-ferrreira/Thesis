3901ec2a6b56120545ddccd00154b74f4bdcd8b2
eduardo-ferrreira <118142217+eduardo-ferrreira@users.noreply.github.com>
2024-10-21 19:27:09 +0100

    Add files via upload

--- reg_rod_calibration.py
+++ reg_rod_calibration.py
@@ -18,7 +18,7 @@ Lambda = [0.0127, 0.0317, 0.116, 0.311, 1.4, 3.87] #decay constants of the percu
 beta = [0.00031, 0.00166, 0.00151, 0.00328, 0.00103, 0.00021] #percursors fractions
 l = 0.000055 # l is the prompt neutron lifetime (Matos et al.)
 
-df1 = pd.read_csv(r'C:\Users\eduar\Documents\Faculdade\Mestrado IST\2º ano\1. Dissertação\teste calibracao br\values_1.csv')
+df1 = pd.read_csv(r'10-2024/21-10-2024/values_8.csv')
 df1['Time'] = df1['Time']-df1['Time'].iloc[0]
 
 t = np.array(df1['Time'])
@@ -38,22 +38,22 @@ def inhour(T):
 T_d = []
 
 for i in range(len(t)):
-    if abs(t[i]-1044)<1e-3:
+    if abs(t[i]-740)<5e-2:
         p0 = p[i]
         #print(p0)
         for j in range(len(p)):
-            if abs(p[j]-2*p0)/(2*p0)<9.5e-4:
+            if abs(p[j]-2*p0)/(2*p0)<5e-4:
                 T_d.append(round(t[j]-t[i],1))
                 #print(p0, p[j], p[j]/p0)
 
-#print(T_d)
+print(T_d)
 
 for i in range(len(t)):
-    if abs(t[i]-1425)<1e-3:
+    if abs(t[i]-1060)<5e-2:
         p0 = p[i]
         #print(p0)
         for j in range(len(p)):
-            if abs(p[j]-2*p0)/(2*p0)<2.5e-4:
+            if abs(p[j]-2*p0)/(2*p0)<5e-4:
                 T_d.append(round(t[j]-t[i],1))
                 #print(p0, p[j], p[j]/p0)
 """for i in range(len(t)):
@@ -63,10 +63,10 @@ for i in range(len(t)):
             if abs(p[j]-1.5*p0)/p0<1.5e-3:
                 T_d.append(t[j]-t[i])
                 #print(p0, p[j], p[j]/p0)"""
-#print(T_d)
+print(T_d)
 
 for i in range(len(t)):
-    if abs(t[i]-1770)<7.44e-2:
+    if abs(t[i]-1370)<7.5e-2:
         p0 = p[i]
         #print(p0)
         for j in range(len(p)):
@@ -83,14 +83,14 @@ for i in range(len(t)):
                 #print(t[j],t[i])
                 #print(p0, p[j], p[j]/p0)"""
 
-#print(T_d)
+print(T_d)
 
 for i in range(len(t)):
-    if abs(t[i]-2080)<5e-2:
+    if abs(t[i]-1670)<7.5e-2:
         p0 = p[i]
         #print(p0)
         for j in range(len(p)):
-            if abs(p[j]-2*p0)/(2*p0)<2.5e-4:
+            if abs(p[j]-2*p0)/(2*p0)<10e-4:
                 T_d.append(round(t[j]-t[i],1))
                 #print(t[j],t[i])
                 #print(p0, p[j], p[j]/p0)
@@ -104,14 +104,14 @@ for i in range(len(t)):
                 #print(t[j],t[i])
                 #print(p0, p[j], p[j]/p0)"""
 
-#print(T_d)
+print(T_d)
 
 for i in range(len(t)):
-    if abs(t[i]-2425)<5e-2:
+    if abs(t[i]-2005)<5e-2:
         p0 = p[i]
         #print(p0)
         for j in range(len(p)):
-            if abs(p[j]-2*p0)/(2*p0)<2.5e-4:
+            if abs(p[j]-2*p0)/(2*p0)<10e-4 and j<30000:
                 T_d.append(round(t[j]-t[i],1))
                 #print(t[j],t[i])
                 #print(p0, p[j], p[j]/p0)
@@ -125,29 +125,29 @@ for i in range(len(t)):
                 #print(t[j],t[i])
                 #print(p0, p[j], p[j]/p0)"""
 
-#print(T_d)
+print(T_d)
 
 for i in range(len(t)):
-    if abs(t[i]-3100)<7.44e-2:
+    if abs(t[i]-2510)<5e-2:
         p0 = p[i]
-        #print(p0)
+        print(p0)
         for j in range(len(p)):
-            if abs(p[j]-1.5*p0)/p0<3e-4:
+            if abs(p[j]-2*p0)/(2*p0)<5e-5:
                 T_d.append(round(t[j]-t[i],1))
                 #print(t[j],t[i])
                 #print(p0, p[j], p[j]/p0)
 
-#print(T_d)
+print(T_d)
 
 period_list = []
 i=0
 p_value = 0
 if len(T_d)==6:
     while i<6:
-        if i == 5:
-            period_list.append(round(T_d[i]/np.log(1.5),1))
-        else:
-            period_list.append(round(T_d[i]/np.log(2),1))
+        #if i == 5:
+        #    period_list.append(round(T_d[i]/np.log(1.5),1))
+        #else:
+        period_list.append(round(T_d[i]/np.log(2),1))
         i+=1
 
     reactivity = [0]
@@ -158,59 +158,59 @@ if len(T_d)==6:
         reactivity.append(p_value)
     print(T_d, '\n', period_list, '\n', reactivity, '\n', integral_reactivity)
 
-reg_rod_rpi = [0,20.2,29.8,44.9,60.2,75.4,100]#
-reactivity_rpi = [0,81,142,233,299,324,331]
+    reg_rod_rpi = [0,20.2,29.8,44.9,60.2,75.4,100]#
+    reactivity_rpi = [0,81,142,233,299,324,331]
 
-reg_rod = [0,20,30,45,60,75,100]
-#integral_reactivity2 = [0,85,149,241,307,334,346]
+    reg_rod = [0, 19.9, 29.7, 45.4, 59.8, 75.1, 100]
+    #integral_reactivity2 = [0,85,149,241,307,334,346]
 
-cs_1 = CubicSpline(reg_rod_rpi, reactivity_rpi)
-cs_2 = CubicSpline(reg_rod, integral_reactivity)
-#cs_3 = CubicSpline(reg_rod, integral_reactivity2)
-#print(integral_reactivity, cs_2(0))
+    cs_1 = CubicSpline(reg_rod_rpi, reactivity_rpi)
+    cs_2 = CubicSpline(reg_rod, integral_reactivity)
+    #cs_3 = CubicSpline(reg_rod, integral_reactivity2)
+    #print(integral_reactivity, cs_2(0))
 
-x=np.linspace(0,100,1000)
-cs_1_list = []
-cs_2_list = []
-#cs_3_list = []
+    x=np.linspace(0,100,100)
+    cs_1_list = []
+    cs_2_list = []
+    #cs_3_list = []
 
-for i in range(len(x)):
-    cs_1_list.append(cs_1(x[i]))
-    cs_2_list.append(cs_2(x[i]))
-#    cs_3_list.append(cs_3(x[i]))
+    for i in range(len(x)):
+        cs_1_list.append(cs_1(x[i]))
+        cs_2_list.append(cs_2(x[i]))
+    #    cs_3_list.append(cs_3(x[i]))
 
-plt.figure(figsize=(10,6))
-plt.plot(x, cs_2_list, label='fissionist')
-plt.plot(x, cs_1_list, label='rpi')
-#plt.plot(x, cs_3_list, label='rpi')
-plt.scatter(reg_rod[1:], integral_reactivity[1:])
-plt.scatter(reg_rod_rpi[1:], reactivity_rpi[1:])
-plt.xlim(0,100)
-plt.ylim(0,350)
-plt.legend()
-plt.xlabel('Rod position (%)')
-plt.ylabel(r'$\rho$ (pcm)')
-plt.savefig('rod_calibration.pdf', dpi=600, bbox_inches='tight')
-plt.show()
+    plt.figure(figsize=(10,6))
+    plt.plot(x, cs_2_list, label='fissionist')
+    plt.plot(x, cs_1_list, label='rpi')
+    #plt.plot(x, cs_3_list, label='rpi')
+    plt.scatter(reg_rod[1:], integral_reactivity[1:])
+    plt.scatter(reg_rod_rpi[1:], reactivity_rpi[1:])
+    plt.xlim(0,100)
+    plt.ylim(0,440)
+    plt.legend()
+    plt.xlabel('Rod position (%)')
+    plt.ylabel(r'$\rho$ (pcm)')
+    #plt.savefig('rod_calibration.pdf', dpi=600, bbox_inches='tight')
+    plt.show()
 
-fig, ax1 = plt.subplots(figsize=(10,6))
-color = 'tab:red'
-ax1.set_xlabel('$t$ (s)')
-ax1.set_ylabel('Power (W)', color='k')
-ax1.set_yscale('log')
-line1, = ax1.plot(t, p, color=color, label='Power')
-ax1.tick_params(axis='y', labelcolor='k')
-ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
-color = 'tab:blue'
-ax2.set_ylabel('$k_{eff}$', color='k')  # we already handled the x-label with ax1
-line2, = ax2.plot(t, k, color='b', label='$k_{eff}$')
-ax2.set_ylim(0.99950, 1.004)
-line3 = ax2.axhline(y=1, xmin=0, xmax=max(t), color='tab:gray', linestyle='--', label='$k_{eff}=1$')
-ax2.tick_params(axis='y', labelcolor='k')
-# Combine legends
-lines = [line1, line2, line3]
-labels = [line.get_label() for line in lines]
-ax1.legend(lines, labels, loc='upper left')
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
-plt.savefig('power_rod_calib.pdf', dpi=600, bbox_inches='tight')
-#plt.show()
+    fig, ax1 = plt.subplots(figsize=(10,6))
+    color = 'tab:red'
+    ax1.set_xlabel('$t$ (s)')
+    ax1.set_ylabel('Power (W)', color='k')
+    ax1.set_yscale('log')
+    line1, = ax1.plot(t, p, color=color, label='Power')
+    ax1.tick_params(axis='y', labelcolor='k')
+    ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+    color = 'tab:blue'
+    ax2.set_ylabel('$k_{eff}$', color='k')  # we already handled the x-label with ax1
+    line2, = ax2.plot(t, k, color='b', label='$k_{eff}$')
+    ax2.set_ylim(0.99950, 1.004)
+    line3 = ax2.axhline(y=1, xmin=0, xmax=max(t), color='tab:gray', linestyle='--', label='$k_{eff}=1$')
+    ax2.tick_params(axis='y', labelcolor='k')
+    # Combine legends
+    lines = [line1, line2, line3]
+    labels = [line.get_label() for line in lines]
+    ax1.legend(lines, labels, loc='upper left')
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    #plt.savefig('power_rod_calib.pdf', dpi=600, bbox_inches='tight')
+    #plt.show()
--- rod_drop_test.py
+++ rod_drop_test.py
@@ -1,19 +1,28 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import pandas as pd
 
 m_rod = 15 # kg
 g = 9.81 # m s ^ -2
 p_water = 1000 # kg m ^ -3
 a= 0.0602 #m
 b=0.021 #m
-r_rod = 0.05 # m
+r_rod = 0.004 # m
 l_rod = 0.629 # m
-A_rod = 0.035*0.004*2+np.pi*(10.5e-3-6.5e-3)**2#0.035*0.021+np.pi*0.0105**2 #np . pi * a * b # m ^2
+A_rod = 0.035*0.004*2+np.pi*(10.5e-3-6.5e-3)**2-np.pi*r_rod**2#0.035*0.021+np.pi*0.0105**2 #np . pi * a * b # m ^2
 V_rod = A_rod * l_rod # m ^3
 c_d = 0.82 # coefficient drag of a long cylinder
-y0_rod = l_rod+l_rod/2# max height position of the center of mass of the rods
+y0_rod = (l_rod+l_rod/2)# max height position of the center of mass of the rods
 vy0_rod = 0 # initial velocity of the rods before scram
+
+df1 = pd.read_csv(r'C:/Users/eduardo.ferreira/Documents/rod_position.csv')
+#df2 = pd.read_csv(r'10-2024/21-10-2024/values_4.csv')
+
+y_taylor = np.array(df1['position'])
+t_taylor = np.array(df1['t fall'])
+
+print(len(y_taylor), len(t_taylor))
 
 def dSdt(t,S):
     y, vy = S
@@ -24,23 +33,23 @@ t=np.linspace(0,0.5,1000)
 S0=(y0_rod, vy0_rod)
 sol=odeint(dSdt, y0=S0, t=t, tfirst=True)
 
-x_taylor=[0, 0.01, 0.06, 0.14, 0.24, 0.3, 0.35, 0.41, 0.43]
-y_taylor=[y0_rod, y0_rod*0.9999, y0_rod*0.98, y0_rod*0.9, y0_rod*0.7, y0_rod*0.55, y0_rod*0.35, y0_rod*0.1, y0_rod*0]
+#x_taylor=[0, 0.01, 0.06, 0.14, 0.24, 0.3, 0.35, 0.41, 0.43]
+#y_taylor=[y0_rod, y0_rod*0.9999, y0_rod*0.98, y0_rod*0.9, y0_rod*0.7, y0_rod*0.55, y0_rod*0.35, y0_rod*0.1, y0_rod*0]
 
 plt.figure(figsize=(10,6))
-plt.plot(t, sol.T[0], label='Runge-Kutta 4$^{th}$ order', color='b')
-plt.scatter(x_taylor,y_taylor, label='Taylor 1$^{st}$ order (hipotético)', color='tab:orange')
-plt.plot(x_taylor,y_taylor,color='tab:orange')#, label='Taylor 1$^{st}$ order')
+plt.plot(t, 100*sol.T[0]/y0_rod, label='Runge-Kutta 4$^{th}$ order', color='b')
+plt.scatter(t_taylor,y_taylor, label='Taylor 1$^{st}$ order', color='tab:orange')
+plt.plot(t_taylor,y_taylor*100,color='tab:orange')#, label='Taylor 1$^{st}$ order')
 #plt.vlines(x=0.430,ymin=0, ymax=max(sol.T[0]), linestyle='--')
 #plt.vlines(x=0.470,ymin=0, ymax=max(sol.T[0]), linestyle='--')
 #plt.hlines(y=0,xmin=0, xmax=max(t), linestyle='--')
-plt.ylim(0,1)
+plt.ylim(0,101)
 plt.xlim(0,max(t))
-plt.ylabel('Rod height (m)')
+plt.ylabel('Rod (%)')
 plt.xlabel('$t$ (s)')
 y1=np.array(0.427)
 y2=np.array(0.467)
-plt.fill_between(x=[0.430,0.470], y1=0, y2=1, where=[0.430,0.470], color='C1', alpha=0.3, label='RPI data')
+plt.fill_between(x=[0.430,0.470], y1=0, y2=101, where=[0.430,0.470], color='C1', alpha=0.3, label='RPI data interval')
 plt.legend()
-plt.savefig('rod_drop.pdf', dpi=600, bbox_inches='tight')
+#plt.savefig('rod_drop.pdf', dpi=600, bbox_inches='tight')
 plt.show()
